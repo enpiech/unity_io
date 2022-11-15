8ecf0a2f965d713f954712da9eae34fcb79c57ec
Huy Phan <enpiech@gmail.com>
2022-11-15 15:25:28 +0700

    REFACTOR: - convert all possible class to struct

--- Packages/Scripts/Runtime/Data/ReadFileRequestDto.cs
+++ Packages/Scripts/Runtime/Data/ReadFileRequestDto.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace Enpiech.IO.Runtime.Data
 {
     [Serializable]
-    public class ReadFileRequestDto
+    public struct ReadFileRequestDto : IEquatable<ReadFileRequestDto>
     {
         [SerializeField]
         private string _key;
@@ -22,5 +22,30 @@ namespace Enpiech.IO.Runtime.Data
         public string Key => _key;
 
         public string FileName => Path.Combine(Application.persistentDataPath, _fileName);
+
+        public bool Equals(ReadFileRequestDto other)
+        {
+            return _key == other._key && _fileName == other._fileName;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ReadFileRequestDto other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_key, _fileName);
+        }
+
+        public static bool operator ==(ReadFileRequestDto left, ReadFileRequestDto right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(ReadFileRequestDto left, ReadFileRequestDto right)
+        {
+            return !left.Equals(right);
+        }
     }
 }
--- Packages/Scripts/Runtime/Data/WriteFileRequestDto.cs
+++ Packages/Scripts/Runtime/Data/WriteFileRequestDto.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace Enpiech.IO.Runtime.Data
 {
     [Serializable]
-    public class WriteFileRequestDto
+    public class WriteFileRequestDto : IEquatable<WriteFileRequestDto>
     {
         [SerializeField]
         private string _key;
@@ -28,5 +28,52 @@ namespace Enpiech.IO.Runtime.Data
         public string FileName => Path.Combine(Application.persistentDataPath, _fileName);
 
         public string SerializedData => _serializedData;
+
+        public bool Equals(WriteFileRequestDto? other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return _key == other._key && _fileName == other._fileName && _serializedData == other._serializedData;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return Equals((WriteFileRequestDto)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_key, _fileName, _serializedData);
+        }
+
+        public static bool operator ==(WriteFileRequestDto? left, WriteFileRequestDto? right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(WriteFileRequestDto? left, WriteFileRequestDto? right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
